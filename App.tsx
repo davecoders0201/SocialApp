b54f97c2635b7782c2a442625bc50d0321ffcdc1
davecoders0201 <davecoder0201@gmail.com>
2023-04-19 14:59:50 +0530

    Include the Navigation of the Home Screen

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Login from './screen/components/Login';
 import Reg from './screen/components/Reg';
-import TabNavigation from './navigation/tabNavigation';
+import Home from './screen/Screens/Home';
 
 // Create a Reference of the Stack which will display in the screen stack wise
 const Stack = createStackNavigator();
@@ -16,7 +16,7 @@ function App() {
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Reg" component={Reg} />
-        <Stack.Screen name="tabNavigation" component={TabNavigation} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
