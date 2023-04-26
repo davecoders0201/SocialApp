953622b4073b0253fa7cdc8e6fccec9d5f73e2ae
davecoders0201 <davecoder0201@gmail.com>
2023-04-26 12:44:49 +0530

    Update Main Function Name

--- screen/externalScreens/NewMessages.js
+++ screen/externalScreens/NewMessages.js
@@ -1,14 +1,14 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 
-const MyMessages = () => {
+const NewMessages = () => {
   return (
     <View>
-      <Text>MyMessages</Text>
+      <Text>NewMessages</Text>
     </View>
   );
 };
 
-export default MyMessages;
+export default NewMessages;
 
 const styles = StyleSheet.create({});
